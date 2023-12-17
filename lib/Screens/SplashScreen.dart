9cabd88e7c6ad0738b7cb89d85339ffc12da6a03
PRAFUL <PRAFUL@LAPTOP>
2023-12-17 21:21:54 +0530

    DAY 1 (17 DEC 2023)

---
+++ lib/Screens/SplashScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('assetName'),fit: BoxFit.cover)
+        ),
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
